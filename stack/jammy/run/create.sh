0e91655123ab8bb3ef8e678498d67feb2eeb5221
dashaun <dashaun@dashaun.com>
2022-12-12 14:55:38 -0600

    :tiny

--- stack/jammy/run/create.sh
+++ stack/jammy/run/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
 
-docker build . -t dashaun/jammy-run \
+docker build . -t dashaun/jammy-run:tiny \
 --build-arg packages="base-files ca-certificates libc6 libssl3 netbase openssl tzdata zlib1g"
