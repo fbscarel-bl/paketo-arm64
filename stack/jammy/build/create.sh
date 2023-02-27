89ed2f7f1dc32682df381b33d1c3e633d49dbb0d
dashaun <dashaun@dashaun.com>
2023-02-27 09:56:48 -0600

    move args into dockerfile

--- stack/jammy/build/create.sh
+++ stack/jammy/build/create.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-docker build . -t dashaun/jammy-build:tiny \
---build-arg sources="deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted universe multiverse \n deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted universe multiverse \n deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-security main restricted universe multiverse" \
---build-arg packages="build-essential ca-certificates curl git jq libgmp-dev libssl3 libyaml-0-2 netbase openssl tzdata xz-utils zlib1g-dev"
+docker build . -t dashaun/jammy-build:tiny
