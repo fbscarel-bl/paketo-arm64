ceb66d70e920b6d97e412105b81f7e4fc7c51e6a
dashaun <dashaun@dashaun.com>
2023-02-27 09:49:38 -0600

    move args into dockerfile

--- stack/jammy/run/create.sh
+++ stack/jammy/run/create.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-docker build . -t dashaun/jammy-run:tiny \
---build-arg packages="base-files ca-certificates libc6 libssl3 netbase openssl tzdata zlib1g"
+docker build . -t dashaun/jammy-run:tiny
